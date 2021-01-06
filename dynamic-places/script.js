51aec20c4cb597d5eaf46395056762b2964d4d04
urandombg <53340072+urandombg@users.noreply.github.com>
2021-01-06 11:54:38 +0200

    Update script.js

--- dynamic-places/script.js
+++ dynamic-places/script.js
@@ -66,7 +66,4 @@ window.onload = () => {
             timeout: 27000,
         }
     );
-  const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
-  console.log(distanceMsg);   // "890 meters"
-  console.log('fff')
 };
