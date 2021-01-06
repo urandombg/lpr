40b1bbde8dfd18c783dd063b67c05f4e2ec4af7d
urandombg <53340072+urandombg@users.noreply.github.com>
2021-01-06 10:53:26 +0200

    Update script.js

--- dynamic-places/script.js
+++ dynamic-places/script.js
@@ -2,8 +2,8 @@
 function loadPlaces(position) {
     const params = {
         radius: 300,    // search places not farther than this value (in meters)
-        clientId: '<your-client-id>',
-        clientSecret: '<your-client-secret>',
+        clientId: 'VLSPLTLJS5ABC3TWIINGHCEYY3DECEHENG4XZBHSETEQTU5D',
+        clientSecret: 'JER4CD3PIFP1S2ZQZLKF0ZWOXWCZUXTPG55RZ2VIWXZUASF5',
         version: '20300101',    // foursquare versioning, required but unuseful for this demo
     };
 
